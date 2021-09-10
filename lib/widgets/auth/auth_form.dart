34c73e2ad8f225e31f5a192167cad20680f43c35
Duong Tung <19522494@gm.uit.edu.vn>
2021-09-10 13:07:42 +0700

    add firebase authentication for sign in & sign up

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
+  AuthForm(this.submitFn);
+
+  final void Function(String email, String password, String username,
+      bool isLogin, BuildContext context) submitFn;
+
   @override
   _AuthFormState createState() => _AuthFormState();
 }
@@ -19,9 +24,13 @@ class _AuthFormState extends State<AuthForm> {
     FocusScope.of(context).unfocus();
     if (isValid) {
       _formKey.currentState.save();
-      print(_userEmail);
-      print(_userName);
-      print(_userPassword);
+      widget.submitFn(
+        _userEmail.trim(),
+        _userPassword.trim(),
+        _userName.trim(),
+        _isLogin,
+        context,
+      );
     }
   }
 
